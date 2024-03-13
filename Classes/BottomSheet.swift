08fc081eb03c5e4fff1ed96f6731202fd96d825f
Bence Tóth <bence.toth.work@icloud.com>
2024-03-13 06:43:03 +0100

    initial commit

---
+++ Classes/BottomSheet.swift
@@ -0,0 +1,18 @@
+//
+//  Bottomsheet.swift
+//  CustomAlertKit
+//
+//  Created by Bence Tóth on 12/03/2024.
+//
+
+import SwiftUI
+
+struct Bottomsheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Bottomsheet()
+}
---
+++ Classes/CustomAlertKit.swift
@@ -0,0 +1,8 @@
+//
+//  CustomAlertKit.swift
+//  CustomAlertKit
+//
+//  Created by Bence Tóth on 12/03/2024.
+//
+
+import Foundation
